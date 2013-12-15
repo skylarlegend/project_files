4efbd874454f456857c8b6e25547dfa1cacee92d
skylarlegend <skylarlegend@gmail.com>
2013-12-15 22:03:49 +0000

    Added python files (till ex21)

---
+++ Python Projects/ex13.py
@@ -0,0 +1,9 @@
+from sys import argv
+
+script, first, second, third = argv
+
+print "The script is called:", script
+first = raw_input ("First variable")
+print first
+print "Your second variable is:", second
+print "Your third variable is:", third
